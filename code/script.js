4e8371348df606b24539fd83b1aab928b318234a
Hankering1716 <116718014+Hankering1716@users.noreply.github.com>
2024-04-22 18:27:29 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,1 +1,14 @@
 
+
+
+
+
+function confirm() {
+    var divMoney = document.getElementById("divMoney")
+    var money = document.getElementById("moneyCount");
+    var inputNumberMoney  = document.getElementById("inputMoney");
+    divMoney.style.display = ""
+
+    document.getElementById("inputDiv").style.display = "none";
+    money.innerHTML = inputNumberMoney.value;
+}
